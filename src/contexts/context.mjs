68bc13d6fbd028905264b6d4bd6837c9602c18c0
Andrea Pandolfo <andrea.pandolfo@gmail.com>
2024-10-27 19:16:19 +0100

    Enhance context management: implement initial state and add state retrieval methods

--- src/contexts/context.mjs
+++ src/contexts/context.mjs
@@ -1,18 +1,62 @@
 // ./src/contexts/context.mjs
 
-class Context {
-    constructor() {
-      this.state = {};
-    }
+import visionFadeModule from '../module.json';
+import config from '../config/config.json';
+import CONST from '../config/constants.mjs';
 
-    set(key, value) {
-      this.state[key] = value;
-    }
-    
-    get(key) {
-      return this.state[key];
-    }
+
+const initialState = {
+  settingsReady: false,
+  module: visionFadeModule,
+  config: config,
+  CONST: CONST
+};
+
+/**
+ * Class representing a context with a state to be
+ * shared across the module.
+ */
+class Context {
+  /**
+   * Create a context.
+   */
+  constructor(initialState) {
+    this.initialState = initialState;
+    this.state = { ...initialState };
   }
-  
-  export const context = new Context();
-  
+
+  /**
+   * Set a value in the context state.
+   * @param {string} key - The key for the state value.
+   * @param {*} value - The value to set.
+   */
+  set(key, value) {
+    this.state[key] = value;
+  }
+
+  /**
+   * Get a value from the context state.
+   * @param {string} key - The key for the state value.
+   * @returns {*} The value associated with the key.
+   */
+  get(key) {
+    return this.state[key];
+  }
+
+  /**
+   * Get the entire context state.
+   * @returns {Object} The context state.
+   */
+  getState() {
+    return this.state;
+  }
+
+  initializeState() {
+    this.state = { ...this.initialState };
+  }
+}
+
+
+const context = new Context(initialState);
+
+export default context

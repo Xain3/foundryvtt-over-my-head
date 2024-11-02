c7ace213de90715501e05d98c11e15e11ff94b8b
Andrea Pandolfo <andrea.pandolfo@gmail.com>
2024-11-02 15:48:59 +0100

    Enhance context state management: add support for nested paths in set and get methods, allowing for more flexible state manipulation

--- src/contexts/context.js
+++ src/contexts/context.js
@@ -31,17 +31,37 @@ class Context {
    * Set a value in the context state.
    * @param {string} key - The key for the state value.
    * @param {*} value - The value to set.
+   * @param {...string} path - The path to the value. If omitted, the key is registered at the top level.
    */
-  set(key, value) {
+  fetchStateAtPath(path) {
+    let obj = this.state;
+    for (let i = 0; i < path.length - 1; i++) {
+      obj = obj[path[i]];
+    }
+    return obj;
+  }
+
+  set(key, value, ...path) {
+    if (path.length > 0) {
+      let obj = this.fetchStateAtPath(path);
+      obj[path[path.length - 1]] = value;
+      return;
+    }
     this.state[key] = value;
   }
+
 
   /**
    * Get a value from the context state.
    * @param {string} key - The key for the state value.
    * @returns {*} The value associated with the key.
+   * @param {...string} path - The path to the value. If omitted, the key is fetched at the top level.
    */
-  get(key) {
+  get(key, ...path) {
+    if (path.length > 0) {
+      let obj = this.fetchStateAtPath(path);
+      return obj;
+    }
     return this.state[key];
   }
 

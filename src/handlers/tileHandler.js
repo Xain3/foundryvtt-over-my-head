a979631b42aca94e9dc2f7e7e36d7c4e8160697e
Andrea Pandolfo <andrea.pandolfo@gmail.com>
2024-12-09 00:49:37 +0100

    Extend TileHandler and TokenHandler from PlaceableHandler for improved functionality and code reuse

--- src/handlers/tileHandler.js
+++ src/handlers/tileHandler.js
@@ -1,7 +1,29 @@
 // ./src/handlers/tileHandler.js
+import PlaceableHandler from './placeableHandler.js';
 
-class TileHandler{
-    
+class TileHandler extends PlaceableHandler {
+    constructor(config, context, utils) {
+        super(config, context, utils);
+        this.placeableType = this.config.CONSTANTS.HANDLERS.TILE.PLACEABLE_TYPE;
+        this.alsoFadeTiles = [];
+    }
+
+    getTiles(updateProperty = true, returnValue = true) {
+        return this.getPlaceables(this.placeableType, updateProperty, returnValue);
+    }
+
+    getAlsoFadeFlag(tile) {
+        return tile.document.getFlag(this.config.MODULE.ID, this.config.FLAGS.ALSOFADE);
+    }
+
+    setOcclusionMode(tile, occlusion) {
+        tile.document.update(occlusion);
+    }
+
+    filterAlsoFadeTiles(tiles = this.placeables) {
+        this.alsoFadeTiles = tiles.filter(tile => this.getAlsoFadeFlag(tile));
+        return this.alsoFadeTiles;
+    }
 }
 
 export default TileHandler;
--- src/handlers/tokenHandler.js
+++ src/handlers/tokenHandler.js
@@ -1,7 +1,41 @@
 // ./src/handlers/tokenHandler.js
 
-class TokenHandler {
 
+import PlaceableHandler from './placeableHandler.js';
+
+class TokenHandler extends PlaceableHandler {
+    constructor(config, context, utils) {
+        super(config, context, utils);
+        this.placeableType = this.config.CONSTANTS.HANDLERS.TOKEN.PLACEABLE_TYPE;
+    }
+
+    getTokens(updateProperty = true, returnValue = true) {
+        return this.getPlaceables(this.placeableType, updateProperty, returnValue);
+    }
+
+    getSelectedTokens() {
+        return this.getSelectedPlaceables();
+    }
+    
+    setCurrentToken(token) {
+        this.setCurrentPlaceable(token);
+    }
+    
+    isUnderReference(token, reference, referenceManager, targetUse = 'center', referenceUse = 'rectangle') {
+        return this.isUnder(token, reference, this, referenceManager, targetUse, referenceUse);
+    }
+    
+    isUnderTile(token, tile, tileManager) {
+        return this.isUnder(token, tile, this, tileManager);
+    }
+
+    isControlled(token) {
+        return token.document.isOwner;
+    }
+
+    isSelected(token) { //WIP
+        return token.isSelected;
+    }
 }
 
 export default TokenHandler;

3d11746d301978c5e20209e1d530a6246f8bfae2
Andrea Pandolfo <andrea.pandolfo@gmail.com>
2024-12-10 15:43:06 +0100

    Add listeners.js as a wrapper class for all listeners

--- src/listeners/listeners.js
+++ src/listeners/listeners.js
@@ -1,38 +1,17 @@
 // ./src/listeners/listeners.js
 
+import StateListener from "./stateListener";
+
 class Listeners {
-    constructor(config, context, utils, gameObject) {
+    constructor(config, context, utils){
         this.config = config;
-        this.logger = utils.logger;
         this.context = context;
-        this.gameObject = gameObject;
+        this.utils = utils;
+        this.stateListener = new StateListener(this.config, this.context, this.utils);
     }
 
-    setupLogState() {
-        Hooks.on(this.config.formatHook(this.config.HOOKS.IN['logState'], 'IN'), () =>{ 
-            this.logger.log('Logging state');
-            this.context.logState()
-        });
-    }
+    run(){
+        this.stateListener.run();        let stateListener = new StateListener(this.config, this.context, this.utils);
 
-    setupLogRemoteState() {
-        Hooks.on(this.config.formatHook(this.config.HOOKS.IN['logRemoteState'], 'IN'), () =>{ 
-            this.logger.log('Logging remote state');
-            this.context.getRemoteLocation()
-        });
-    }
-
-    setupGetRemoteState() {
-        Hooks.on(this.config.formatHook(this.config.HOOKS.IN['getRemoteState'], 'IN'), () =>{ 
-            this.logger.log('Getting remote state');
-            let state = this.context.getState()
-            
-        });
-    }
-
-    run() {
-        this.setupLogState();
     }
 }
-
-export default Listeners;

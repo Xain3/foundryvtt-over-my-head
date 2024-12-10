74b5723eda34dc59ca61ee6eaaab9f0b0504421d
Andrea Pandolfo <andrea.pandolfo@gmail.com>
2024-12-10 15:43:45 +0100

    Add StateListener class to manage state logging and retrieval

---
+++ src/listeners/stateListener.js
@@ -0,0 +1,37 @@
+// ./src/listeners/listeners.js
+
+import Listener from "../baseClasses/listener.js";
+
+class StateListener extends Listener {
+    constructor(config, context, utils) {
+        super(config, context, utils);
+    }
+
+    setupLogState() {
+        Hooks.on(this.config.formatHook(this.config.HOOKS.IN['logState'], 'IN'), () =>{ 
+            this.logger.log('Logging state');
+            this.context.logState()
+        });
+    }
+
+    setupLogRemoteState() {
+        Hooks.on(this.config.formatHook(this.config.HOOKS.IN['logRemoteState'], 'IN'), () =>{ 
+            this.logger.log('Logging remote state');
+            this.context.getRemoteLocation()
+        });
+    }
+
+    setupGetRemoteState() {
+        Hooks.on(this.config.formatHook(this.config.HOOKS.IN['getRemoteState'], 'IN'), () =>{ 
+            this.logger.log('Getting remote state');
+            let state = this.context.getState()
+            
+        });
+    }
+
+    run() {
+        this.setupLogState();
+    }
+}
+
+export default StateListener;

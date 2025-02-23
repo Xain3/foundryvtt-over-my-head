6ee55c08167a217cf908609dc388c792b7e2bcd5
Andrea Pandolfo <andrea.pandolfo@gmail.com>
2025-02-23 19:29:08 +0100

    Refactor Listeners and StateListener to ensure consistent token handling and improve hook formatting

--- src/listeners/listeners.js
+++ src/listeners/listeners.js
@@ -39,8 +39,8 @@ class Listeners extends Listener {
     constructor(config, context, utils, handlers){
         super(config, context, utils, handlers);
         this.state = new StateListener(this.config, this.context, this.utils, this.handlers);
-        this.token = new TokenListener(this.config, this.context, this.utils, this.handlers);
         this.tile = new TileListener(this.config, this.context, this.utils, this.handlers);
+        this.token = new TokenListener(this.config, this.context, this.utils, this.handlers);
     }
 
     /**
@@ -51,8 +51,8 @@ class Listeners extends Listener {
      */
     run(){
         this.state.run();
-        this.token.run();
         this.tile.run();
+        this.token.run();
     }
 }
 

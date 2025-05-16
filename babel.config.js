f5d2e7781b9e3f194a0b1672cab35ac8dcf7c59e
Andrea Pandolfo <andrea.pandolfo@gmail.com>
2025-05-16 13:21:05 +0200

    chore: Add Babel and JS config files
    - Create babel.config.js for Babel configuration
    - Set up presets, source maps, and module resolver aliases
    - Create jsconfig.json for TypeScript/JavaScript project configuration
    - Define base URL and paths for module resolution

---
+++ babel.config.js
@@ -0,0 +1,35 @@
+// babel.config.js
+
+module.exports = {
+  presets: [
+    ['@babel/preset-env', {
+      targets: {
+        node: 'current',
+      },
+    }],
+  ],
+  sourceMaps: 'inline', // Correct way to enable inline source maps
+  retainLines: true, // Correct way to retain line numbers
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          '@': './src',
+          '@baseClasses': './src/baseClasses',
+          '@config': './src/config',
+          '@contexts': './src/contexts',
+          '@data': './src/data',
+          '@handlers': './src/handlers',
+          '@utils': './src/utils',
+          '@listeners': './src/listeners',
+          '@integrationTests': './tests/integration',
+          '@mocks': './tests/mocks',
+          '@scripts': './scripts',
+          '@manifest': './module.json',
+        },
+      },
+    ],
+  ],
+};

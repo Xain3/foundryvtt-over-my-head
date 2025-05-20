d238d0ccbd18e452ba6b5e0c05f8794fc5b5ccd3
Andrea Pandolfo <andrea.pandolfo@gmail.com>
2025-05-20 13:34:13 +0200

    refactor: move OverMyHead class to separate file
    - Extracted the OverMyHead class from main.js into a new file overMyHead.js.
    - Updated main.js to import the OverMyHead class from the new file.

--- src/main.js
+++ src/main.js
@@ -1,32 +1,4 @@
-import manifest from "@manifest";
-
-class OverMyHead {
-  constructor() {
-    this.manifest = manifest;
-    this.#unpackManifest();
-  }
-
-  #unpackManifest() {
-    try {
-      for (const [key, value] of Object.entries(this.manifest)) {
-        this[key] = value;
-      }
-    } catch (error) {
-      console.error("Error unpacking Over My Head manifest:", error);
-      throw error;
-    }
-  }
-
-  async init() {
-    try {
-      // Initialization logic to go here
-      console.log(`${this.title} v${this.version} initialized.`);
-    } catch (error) {
-      console.error(`Error initializing ${this.title} v${this.version}: `, error);
-      throw error;
-    }
-  }
-}
+import OverMyHead from './OverMyHead.js';
 
 function main() {
   const omh = new OverMyHead();
---
+++ src/overMyHead.js
@@ -0,0 +1,34 @@
+import manifest from "@manifest";
+import constants from "@constants";
+
+class OverMyHead {
+  constructor() {
+    this.manifest = manifest;
+    this.constants = constants;
+    this.#unpackManifest();
+  }
+
+  #unpackManifest() {
+    try {
+      for (const [key, value] of Object.entries(this.manifest)) {
+        this[key] = value;
+      }
+    } catch (error) {
+      console.error("Error unpacking Over My Head manifest:", error);
+      throw error;
+    }
+  }
+
+  async init() {
+    try {
+
+      // Initialization logic to go here
+      console.log(`${this.title} v${this.version} initialized.`);
+    } catch (error) {
+      console.error(`Error initializing ${this.title} v${this.version}: `, error);
+      throw error;
+    }
+  }
+}
+
+export default OverMyHead;

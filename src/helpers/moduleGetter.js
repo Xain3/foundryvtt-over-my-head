ce3927ed990946d1a61161645453667ded308b5c
Andrea Pandolfo <andrea.pandolfo@gmail.com>
2025-05-26 21:54:21 +0200

    docs: Update documentation for getModule function
    - Added detailed JSDoc comments for the getModule function.
    - Clarified parameter types and return values.
    - Specified error conditions for better understanding.

--- src/helpers/moduleGetter.js
+++ src/helpers/moduleGetter.js
@@ -3,6 +3,15 @@ import { resolvePath } from "./resolvePath.js";
 
 const MODULES_LOCATION = constants.defaultFoundryModulesLocation || 'game.modules';
 
+/**
+ * Retrieves a module by name from the global modules collection.
+ *
+ * @param {string} moduleName - The name of the module to retrieve
+ * @param {object} [globalNamespace=globalThis] - The global namespace object to search in
+ * @returns {*} The requested module object
+ * @throws {TypeError} When moduleName is not a string or globalNamespace is not an object
+ * @throws {Error} When the module is not found or the modules collection is invalid
+ */
 export const getModule = (moduleName, globalNamespace = globalThis) => {
   if (typeof moduleName !== 'string') {
     throw new TypeError('moduleName must be a string');

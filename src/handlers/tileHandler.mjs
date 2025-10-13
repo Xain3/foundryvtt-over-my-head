bf72172266c2c596fe96edb05cb04e597bb1c783
Xain3 <andrea.pandolfo@gmail.com>
2025-10-13 22:24:48 +0000

    feat: integrate barebones TileListener into TileHandler
    Add import and instantiation of TileListener in TileHandler constructor.
    This wires up the listener for tile-related events, building on the
    previously added helper class.
    
    - src/handlers/tileHandler.mjs

--- src/handlers/tileHandler.mjs
+++ src/handlers/tileHandler.mjs
@@ -1,5 +1,6 @@
 import PlaceableHandler from './placeableHandler.mjs';
 import TileChecker from './placeableHelpers/tileChecker.mjs';
+import TileListener from './placeableHelpers/tokenListener.mjs';
 
 const PLACEABLE_TYPE = 'tiles';
 
@@ -7,6 +8,7 @@ class TileHandler extends PlaceableHandler {
   constructor(config, context, utils, type = PLACEABLE_TYPE) {
     super(config, context, utils, type);
     this.checker = new TileChecker(config, context, utils, this.getter);
+    this.listener = new TileListener();
   }
 }
 

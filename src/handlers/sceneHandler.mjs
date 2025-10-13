047c92e22633cd4541d8a64a5e97a483d713d830
Xain3 <andrea.pandolfo@gmail.com>
2025-10-13 22:24:48 +0000

    feat: add barebones SceneHandler with listener integration
    Introduce SceneHandler class that instantiates a SceneListener. This sets up
    the basic structure for scene management, depending on the previously added
    SceneListener helper.
    
    - src/handlers/sceneHandler.mjs

---
+++ src/handlers/sceneHandler.mjs
@@ -0,0 +1,9 @@
+import SceneListener from './sceneHelpers/sceneListener.mjs';
+
+class SceneHandler {
+  constructor() {
+    this.listener = new SceneListener();
+  }
+}
+
+export default SceneHandler;

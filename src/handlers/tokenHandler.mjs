ce2185416c4bc379a4a340582437b6a4ae87ad33
Xain3 <andrea.pandolfo@gmail.com>
2025-10-13 22:24:49 +0000

    feat: integrate barebones TokenListener into TokenHandler
    Add import and instantiation of TokenListener in TokenHandler constructor.
    This enables listener functionality for token handling, utilizing the
    shared helper class.
    
    - src/handlers/tokenHandler.mjs

--- src/handlers/tokenHandler.mjs
+++ src/handlers/tokenHandler.mjs
@@ -1,5 +1,6 @@
 import PlaceableHandler from './placeableHandler.mjs';
 import TokenChecker from './placeableHelpers/tokenChecker.mjs';
+import TokenListener from './placeableHelpers/tileListener.mjs';
 
 const PLACEABLE_TYPE = 'token';
 
@@ -7,6 +8,7 @@ class TokenHandler extends PlaceableHandler {
   constructor(config, context, utils) {
     super(config, context, utils, PLACEABLE_TYPE);
     this.checker = new TokenChecker(config, context, utils, this.getter);
+    this.listener = new TokenListener();
   }
 }
 
